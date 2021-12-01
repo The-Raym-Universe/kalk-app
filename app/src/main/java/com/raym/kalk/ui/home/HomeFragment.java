899390113741c289e90953b0632eb6da18f6396f
Leo-Chan01 <ephraimleo16@gmail.com>
2021-12-01 06:31:39 +0100

    UX Enhancement, change of profile icon and title to home icon and title.

--- app/src/main/java/com/raym/kalk/ui/home/HomeFragment.java
+++ app/src/main/java/com/raym/kalk/ui/home/HomeFragment.java
@@ -1,4 +1,4 @@
-package com.raym.kalk.ui.profile;
+package com.raym.kalk.ui.home;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -8,16 +8,16 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import com.raym.kalk.R;
 
-public class ProfileFragment extends Fragment {
+public class HomeFragment extends Fragment {
 
-    public ProfileFragment(){
+    public HomeFragment(){
 
     }
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        return inflater.inflate(R.layout.fragment_home, container, false);
     }
 
 }
